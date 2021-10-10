800a6cd70a6593c18bd508aeab38489a482d4ae3
it-laia <it@laia.com.ar>
2021-10-10 19:54:00 -0300

    Carga inicial

---
+++ esenciales.js
@@ -0,0 +1,36 @@
+document.getElementById("header").innerHTML = `
+        <div>
+            <a id="logo" href="index.html"><h2>Alan Pirotta</h2></a>  <!--logo de mi nombre, más grande y a la izq-->
+        </div>
+        <nav>
+            <a class="nav pag_activa" href="index.html">SOBRE MI</a>
+            <a class="nav" href="conocimientos.html">CONOCIMIENTOS</a>
+            <a class="nav" href="experiencia_laboral.html">EXPERIENCIA LABORAL</a>
+            <a class="nav" href="contacto.html">CONTACTO</a>
+        </nav>
+        `
+
+
+document.getElementById("footer").innerHTML = `
+        <h3 class="footer_logo footer_start">
+            Alan Pirotta
+        </h3>
+        <h4 class="footer_title">
+            Whatsapp
+        </h4>
+        <h4 class="footer_title">
+            Escríbeme
+        </h4>
+        <h4 class="footer_title footer_end">
+            Sígueme
+        </h4>
+        <h4 class="footer_title footer_start">
+            Octubre 2021
+        </h4>
+        <a class="footer_link" href="https://wa.me/541159659368" target="_blank">(54)11 5965-9368</a>
+        <a class="footer_link" href="mailto:alanupirotta@gmail.com">alanupirotta@gmail.com</a>
+        <div class="footer_link footer_end">
+            <a href="https://www.linkedin.com/in/alan-uriel-pirotta/" class="fa fa-linkedin"  target="_blank"></a>
+            <a href="https://github.com/alanpirotta" class="fa fa-github"  target="_blank"></a>
+        </div>
+        `
---
+++ validar_form.js
@@ -0,0 +1,43 @@
+var form = document.getElementById('contact_form'), 
+    elements = form.elements, 
+    button = document.getElementById('contact_form_button');
+
+var validateName = function (e) {
+    if (form.name.value == 0) {
+        alert("Complete el campo nombre");
+        e.preventDefault();
+    }
+};
+    
+function validateMessage(e){
+    if(form.message.value == 0){
+        alert("Escriba un mensaje");
+        e.preventDefault();          
+    }
+};
+
+function validateRadio (e){
+    //si fuera más opciones, se haría con un for
+    if(form.subject_type[0].checked == true || form.subject_type[1].checked == true ){
+    }else{
+        alert("Selecciona el tema del mensaje");
+        e.preventDefault();
+    };
+};
+
+function validateMail (e){
+    if(form.mail.value == 0){
+        alert("Esciba un mail existente")
+        e.preventDefault();
+    }
+};
+
+var validate = function (e) {
+    validateName(e);
+    validateMessage(e);
+    validateRadio(e);
+    validateMail(e);
+
+};
+
+form.addEventListener("submit", validate);
